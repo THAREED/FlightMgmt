d334c53ea1051489aa6286b4a1f686cc07669c1d
THAREED <tdaengchaisri@gmail.com>
2018-12-05 00:10:53 +0700

    for assignment 3

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ app.use('/static',express.static(path.join(__dirname, '/public')));
 app.get('/', function(req,res)
 {
   console.log("request: searchFlight as a default page.");
-  res.sendFile(path.join(__dirname,'searchFlight.html'));
+  // res.sendFile(path.join(__dirname,'searchFlight.html'));
+  res.redirect('/searchFlight.html');
 });
 
 app.get('/searchFlight.html', function(req,res)
@@ -31,17 +32,17 @@ app.get('/flightInfo.json', function(req,res)
   res.sendFile(path.join(__dirname,'flightInfo.json'));
 });
 
-// app.get('/cssStyle.css', function(req,res)
-// {
-//   console.log("request: "+req.url);
-//   res.sendFile(path.join(__dirname,'cssStyle.css'));
-// });
+app.get('/passengerdetail.html', function(req,res)
+{
+  console.log("request: "+req.url);
+  res.sendFile(path.join(__dirname,'passengerdetail.html'));
+});
 
-// app.get('/flight.js', function(req,res)
-// {
-//   console.log("request: "+req.url);
-//   res.sendFile(path.join(__dirname,'flight.js'));
-// });
+app.get('/passengerdetail2.html', function(req,res)
+{
+  console.log("request: "+req.url);
+  res.sendFile(path.join(__dirname,'passengerdetail2.html'));
+});
 
 app.listen(3000, function()
 {
